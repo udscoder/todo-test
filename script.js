e5609036570f7c6bf2ccaa97259a5683a23da447
ulugbek <udscoder@gmail.com>
2023-01-30 20:24:54 +0500

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const savingsPage = document.querySelector('.savings_page'),
+    let savingsPage = document.querySelector('.savings_page'),
+        titles = document.querySelectorAll('.title'),
         enterInput = document.querySelector('.entering_input'),
-        enterBtn = document.querySelector('.enter_btn')
+        enterBtn = document.querySelector('.enter_btn'),
+        myBtn = document.getElementById('myBtn');
+
+
+
+    let theme;
+
+
+
+    function themes() {
+
+        myBtn.addEventListener('click', () => {
+
+            let setTheme = document.body;
+
+            setTheme.classList.toggle('dark-mode');
+
+
+            if(setTheme.classList.contains('dark-mode')) {
+                theme = 'Dark'
+
+            }
+            else {
+                theme = 'Light'
+            }
 
 
 
 
+
+            localStorage.setItem('PageTheme', JSON.stringify(theme))
+        })
+
+
+
+
+        let GetTheme = JSON.parse(localStorage.getItem('PageTheme'))
+
+        if(GetTheme === 'Dark') {
+            document.body.classList = 'dark-mode';
+
+        }
+    }
+    themes()
+
+
+    function applyStyles(savingsDivStyle, deleteBtn) {
+            savingsDivStyle.style.cssText =
+                `
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #fff;
+            margin-bottom: 5px;
+            padding: 10px;
+            font-size: 20px;
+            `;
+            deleteBtn.style.cssText = `
+        background-color: dodgerblue;
+        padding: 5px;
+        border-radius: 4px;
+        border: none;
+        `
+
+    }
 
 
 
@@ -15,31 +76,54 @@ window.addEventListener('DOMContentLoaded', () => {
             alert("Enter task")
         }
         else {
-            let savingsDiv = document.createElement('div');
-
-            let savingsDivStyle = {
-                width: 200,
-                height: 10,
-                backgroundColor: '#fff',
-                marginTop: 10
-            }
 
 
-            savingsDiv.innerHTML = enterInput.value;
-
+            let savingsDiv = document.createElement('div'),
+                text = document.createElement('div');
 
             savingsPage.appendChild(savingsDiv);
-            savingsDiv.style = savingsDivStyle.backgroundColor;
+            savingsDiv.appendChild(text)
+
+            text.innerHTML = enterInput.value;
+
+            enterInput.value = '';
+            deleteBtn(savingsDiv)
 
 
 
-            console.log(savingsDivStyle)
+            let localItem = localStorage.getItem('savingsDiv')
+            if (localItem === null) {
+                savingsDiv = []
+            }
+            else {
+                savingsDiv = localItem
+            }
+            localStorage.setItem('savingsDiv', savingsDiv.toString());
         }
     }
 
+
+    function deleteBtn(savingDiv) {
+        let deleteBtn = document.createElement('button');
+
+        deleteBtn.innerHTML = 'Delete'
+        savingDiv.appendChild(deleteBtn)
+
+        applyStyles(savingDiv, deleteBtn)
+
+        deleteBtn.addEventListener('click', () => {
+            savingDiv.remove();
+        })
+
+    }
+
+
+
+function clicks() {
     enterBtn.addEventListener('click', () => {
-        createTodo();
+        createTodo()
     })
-
-
+}
+  clicks()
 })
+
