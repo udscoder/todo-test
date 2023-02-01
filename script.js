10ce483d09245cb7cbdb79091d8d893b9b8c94c6
ulugbek <udscoder@gmail.com>
2023-02-01 21:35:07 +0500

    changing

--- script.js
+++ script.js
@@ -1,129 +1,161 @@
 window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.title');
     let savingsPage = document.querySelector('.savings_page'),
-        titles = document.querySelectorAll('.title'),
         enterInput = document.querySelector('.entering_input'),
-        enterBtn = document.querySelector('.enter_btn'),
-        myBtn = document.getElementById('myBtn');
+        enterBtn = document.querySelectorAll('.myBtn'),
+        theme = document.querySelector('.todo_list'),
+        darkMode = localStorage.getItem('dark-mode'),
+        sub = document.querySelectorAll('.bre')
 
-
-
-    let theme;
-
-
-
-    function themes() {
-
-        myBtn.addEventListener('click', () => {
-
-            let setTheme = document.body;
-
-            setTheme.classList.toggle('dark-mode');
-
-
-            if(setTheme.classList.contains('dark-mode')) {
-                theme = 'Dark'
-
-            }
-            else {
-                theme = 'Light'
-            }
-
-
-
-
-
-            localStorage.setItem('PageTheme', JSON.stringify(theme))
-        })
-
-
-
-
-        let GetTheme = JSON.parse(localStorage.getItem('PageTheme'))
-
-        if(GetTheme === 'Dark') {
-            document.body.classList = 'dark-mode';
-
+    const applyStyle = (domNode, styleObject) => {
+        for (const property in styleObject) {
+            domNode.style[property] = styleObject[property];
         }
     }
-    themes()
 
+ const domNodes = [];
+    const styleTwo = (domNodes, styleObject) => {
+        Array.from(domNodes).forEach(domNode => {
+            for (const property in styleObject) {
+                domNode.style[property] = styleObject[property];
+            }
+        })
+    }
 
-    function applyStyles(savingsDivStyle, deleteBtn) {
-            savingsDivStyle.style.cssText =
-                `
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            background-color: #fff;
-            margin-bottom: 5px;
-            padding: 10px;
-            font-size: 20px;
-            `;
-            deleteBtn.style.cssText = `
-        background-color: dodgerblue;
-        padding: 5px;
-        border-radius: 4px;
-        border: none;
-        `
+    const styleObject = {
 
+        darkMode: {
+            backgroundColor: '#000',
+            color: '#fff',
+            btn: {
+                backgroundColor: 'red'
+            },
+            subtitle: {
+                color: 'red'
+            }
+
+        },
+        lightMode:{
+            backgroundColor: '#fff',
+            color: '#000',
+            subtitle: {
+                color: 'black'
+            }
+        },
+        savingsDiv: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: 'lightgrey',
+            color: '#000',
+            marginBottom: '5px',
+            padding: '10px',
+            fontsize: '20px'
+        },
+        deleteBtn: {
+            backgroundColor: 'dodgerblue',
+            padding: '5px',
+            borderRadius: '5px',
+            border: 'none'
+        }
     }
 
 
+    const enableDarkMode = () => {
+        applyStyle(theme, styleObject.darkMode)
+        styleTwo(sub, styleObject.darkMode.subtitle)
+        enterBtn[1].classList.remove("dark-mode-toggle");
+        localStorage.setItem("dark-mode", "enabled");
+    };
+    const disableDarkMode = () => {
+        applyStyle(theme, styleObject.lightMode)
+        styleTwo(sub, styleObject.lightMode.subtitle)
 
-    function createTodo() {
+        enterBtn[1].classList.add("dark-mode-toggle");
+        localStorage.setItem("dark-mode", "disabled");
+    };
+
+    if (darkMode === "enabled") {
+        enableDarkMode();
+    }
+
+    enterBtn[1].addEventListener("click", (e) => {
+        darkMode = localStorage.getItem("dark-mode");
+        if (darkMode === "disabled") {
+            enableDarkMode();
+
+        } else {
+            disableDarkMode();
+        }
+    });
+
+
+
+
+    const createDeleteBtn = (savingDiv) => {
+        let deleteBtn = document.createElement('button');
+
+        deleteBtn.innerHTML = 'Delete'
+
+        applyStyle(deleteBtn, styleObject.deleteBtn)
+
+        savingDiv.appendChild(deleteBtn)
+
+        deleteBtn.addEventListener('click', () => {
+            savingDiv.remove();
+        })
+    }
+
+
+    const createTodo = () => {
 
         if(enterInput.value.trim() === '') {
             alert("Enter task")
         }
         else {
-
-
             let savingsDiv = document.createElement('div'),
                 text = document.createElement('div');
 
             savingsPage.appendChild(savingsDiv);
             savingsDiv.appendChild(text)
 
+            applyStyle(savingsDiv, styleObject.savingsDiv)
+
             text.innerHTML = enterInput.value;
 
             enterInput.value = '';
-            deleteBtn(savingsDiv)
-
-
-
-            let localItem = localStorage.getItem('savingsDiv')
-            if (localItem === null) {
-                savingsDiv = []
-            }
-            else {
-                savingsDiv = localItem
-            }
-            localStorage.setItem('savingsDiv', savingsDiv.toString());
+            createDeleteBtn(savingsDiv)
         }
     }
 
 
-    function deleteBtn(savingDiv) {
-        let deleteBtn = document.createElement('button');
 
-        deleteBtn.innerHTML = 'Delete'
-        savingDiv.appendChild(deleteBtn)
-
-        applyStyles(savingDiv, deleteBtn)
-
-        deleteBtn.addEventListener('click', () => {
-            savingDiv.remove();
-        })
-
-    }
-
-
-
-function clicks() {
-    enterBtn.addEventListener('click', () => {
+    enterBtn[0].addEventListener('click', () => {
         createTodo()
     })
-}
-  clicks()
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
